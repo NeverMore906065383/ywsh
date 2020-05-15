0d839ae09341235a5c44b8f3e43dcf9b8d47597e
liyongchao <liyongchao@whyhowinfo.com>
2020-05-15 17:46:14 +0800

    初始化

---
+++ app/src/main/java/com/ywsh/YWSHApp.kt
@@ -0,0 +1,19 @@
+package com.ywsh
+
+import android.app.Application
+
+/**
+ * Copyright, 2020, WhyHow info, All right reserved.
+ *
+ * @author Administrator
+ *  Mail: liyongchao@whyhowinfo.com
+ * Created Time: 2020/5/15
+ * Descroption:
+ */
+class YWSHApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+                //do sth. init
+    }
+}
